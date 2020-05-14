ea466f370dd21dbca6302376adac7d63215c9df4
Darun Arumugham <38879339+Darunaru@users.noreply.github.com>
2020-05-14 22:27:48 +0530

    Update CoreCode.py

--- CoreCode.py
+++ CoreCode.py
@@ -110,3 +110,12 @@ def nltkk():
               sum1 = sum([vec1[x]**2 for x in vec1.keys()])
               sum2 = sum([vec2[x]**2 for x in vec2.keys()])
               denominator = math.sqrt(sum1) * math.sqrt(sum2)
+               
+              if not denominator:
+                 return 0.0
+              else:
+                 return float(numerator) / denominator
+
+         def text_to_vector(text):
+              words = WORD.findall(text)
+              return Counter(words)
