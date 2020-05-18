9d0bc4bd161a794e2e1be8dcfff84b45aed6a6ab
Darun Arumugham <38879339+Darunaru@users.noreply.github.com>
2020-05-18 21:20:23 +0530

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -8,3 +8,8 @@ census = pd.read_csv("dataset.csv")
 x_data = census.drop('RESULT',axis=1)
 y_labels = census['RESULT']
 X_train, X_test, y_train, y_test = train_test_split(x_data,y_labels,test_size=0.3,random_state=101)
+
+attentive = tf.feature_column.categorical_column_with_vocabulary_list("ATTENTIVE", ["yes", "no"])
+embedded_attentive = tf.feature_column.embedding_column(attentive, dimension=2)
+nlp = tf.feature_column.numeric_column('NLP')
+session_time = tf.feature_column.numeric_column('SESSION_TIME')
